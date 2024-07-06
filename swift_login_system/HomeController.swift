b97ec2e09a71f7ad6346169091effa06e1ad19ee
Pchela <lyudmilalittlepchela@gmail.com>
2024-07-06 17:07:11 +0300

    controllers

---
+++ swift_login_system/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeController.swift
+//  swift_login_system
+//
+//  Created by Лю Пчела on 06.07.2024.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPurple
+    }
+}
---
+++ swift_login_system/LoginController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginController.swift
+//  swift_login_system
+//
+//  Created by Лю Пчела on 06.07.2024.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
