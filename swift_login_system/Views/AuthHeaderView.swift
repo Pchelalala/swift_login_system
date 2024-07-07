df71279ed82e10149e83bd7c11630f237153d1f0
Pchela <lyudmilalittlepchela@gmail.com>
2024-07-07 15:17:05 +0300

    AuthHeaderView

--- swift_login_system/Views/AuthHeaderView.swift
+++ swift_login_system/Views/AuthHeaderView.swift
@@ -8,13 +8,63 @@
 import UIKit
 
 class AuthHeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - UI Components
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(named: "logo")
+        iv.tintColor = .white
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 26, weight: .bold)
+        label.text = "Error"
+        return label
+    }()
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.text = "Error"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    init(title: String, subTitle: String){
+        super.init (frame: .zero)
+        self.titleLabel.text = title
+        self.subTitleLabel.text = subTitle
+        
+        self.setupUI()
     }
-    */
-
+    
+    required init?(coder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        self.addSubview(logoImageView)
+        self.addSubview(titleLabel)
+        self.addSubview(titleLabel)
+        
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.logoImageView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor, constant: 16),
+            self.logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.logoImageView.widthAnchor.constraint(equalToConstant: 90),
+            self.logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor)
+        ])
+    }
 }
