e5b088fd3984c700faa4a9eb65596e3006847ec0
Pchela <lyudmilalittlepchela@gmail.com>
2024-07-07 15:11:49 +0300

    AuthHeaderView

---
+++ swift_login_system/Views/AuthHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthHeaderView.swift
+//  swift_login_system
+//
+//  Created by Лю Пчела on 07.07.2024.
+//
+
+import UIKit
+
+class AuthHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
