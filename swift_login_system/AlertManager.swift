8590e7feb90dd050f82d0d2c31ed7fe24b141b3e
Pchela <lyudmilalittlepchela@gmail.com>
2024-07-17 19:06:18 +0300

    alert manager

--- swift_login_system/AlertManager.swift
+++ swift_login_system/AlertManager.swift
@@ -5,4 +5,20 @@
 //  Created by Лю Пчела on 17.07.2024.
 //
 
-import Foundation
+import UIKit
+
+class AlertManager {
+    
+    private static func showBasicAlert(on vc: UIViewController, title: String, message: String?) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            vc.present(alert, animated: true)
+        }
+    }
+}
+
+// MARK: - Message
+extension AlertManager {
+    
+}
